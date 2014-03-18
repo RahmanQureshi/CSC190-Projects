0b2c8d6ad02c0f590b7a62b448d7570503e34dd1
Rahman Qureshi <rahman.a.qureshi@gmail.com>
2014-03-18 12:44:27 -0400

    naming issue resolved

---
+++ linkedlist/LinkedList.c
@@ -0,0 +1,339 @@
+#include "LinkedList.h"
+
+int CreateLinkedList(LinkedListPTR *linkedListHandle)
+{
+	// Create the Linked List
+	LinkedListPTR mLinkedList = (LinkedListPTR) malloc(sizeof(LinkedList));
+
+	if(mLinkedList == NULL){
+		printf("Memory for LinkedList could not be allocated\n");
+		return FATAL_ERROR;
+	}
+
+	mLinkedList->head = NULL;
+	mLinkedList->size = 0;
+
+	SetComparatorLinkedList(mLinkedList, DefaultComparator);
+	SetPrinterLinkedList(mLinkedList, DefaultPrinter);
+
+	*linkedListHandle = mLinkedList;
+
+	return OK;
+}
+
+int DestroyLinkedList(LinkedListPTR *linkedListHandle)
+{
+	if(linkedListHandle==NULL || *linkedListHandle==NULL){
+		printf("LinkedList pointer is NULL\n");
+		return ERROR;
+	}
+
+	LinkedListPTR linkedList = *linkedListHandle;
+
+	free(linkedList);
+
+	linkedList = NULL;
+
+	return OK;
+}
+
+int DestroyNodesLinkedList(LinkedListPTR linkedList)
+{
+	if(linkedList==NULL){
+		printf("LinkedList pointer is NULL\n");
+		return ERROR;
+	}
+
+	unsigned int size = linkedList->size;
+	int i;
+	NodePTR next = linkedList->head;
+
+	for(i=0; i<size; i++){
+		NodePTR temp = next->next;
+		free(next);
+		next = NULL;
+		next = temp;
+	}
+
+	linkedList->size = 0;
+
+	return OK;
+}
+
+
+int DestroyNodesAndDataLinkedList(LinkedListPTR linkedList)
+{
+	if(linkedList==NULL){
+		printf("LinkedList pointer is NULL\n");
+		return ERROR;
+	}
+
+	unsigned int size = linkedList->size;
+	int i;
+	NodePTR next = linkedList->head;
+
+	for(i=0; i<size; i++){
+		NodePTR temp = next->next;
+		if(next->data==NULL){
+			printf("Node data set to null - modified outside of linked list\n");
+		}else{
+			free(next->data);
+		}
+		next->data = NULL;
+		free(next);
+		next= NULL;
+		next = temp;
+	}
+
+	linkedList->size = 0;
+
+	return OK;
+}
+
+int AppendLinkedList(LinkedListPTR linkedList, void *data)
+{
+	if(linkedList==NULL){
+		printf("LinkedList pointer is NULL\n");
+		return ERROR;
+	}
+
+	// Initialize new object. It will become the next head
+	NodePTR newNode;
+	NodePTR curHead = linkedList->head;
+	CreateNode(&newNode, data, curHead);
+
+	if(newNode==NULL){
+		printf("Could not allocate memory for node\n");
+		return FATAL_ERROR;
+	}
+
+	linkedList->head = newNode;
+
+	linkedList->size = linkedList->size + 1; // Everything OK, increase
+
+	return OK;
+}
+
+int InsertSortedLinkedList(LinkedListPTR linkedList, void* data)
+{
+
+	if(linkedList->comparator == NULL){
+		printf("Comparator is not set\n");
+		return ERROR;
+	}
+
+	// Create the new node
+	NodePTR newNode;
+	CreateNode(&newNode, data, NULL);
+	if(newNode==NULL){
+		printf("Could not allocate memory for new node\n");
+		return FATAL_ERROR;
+	}
+
+	Comparator comparator = linkedList->comparator;
+
+	// Get the current head
+	NodePTR curNode = linkedList->head;
+
+	linkedList->size = linkedList->size + 1;
+
+	if(curNode==NULL){ // If the list is empty
+		linkedList->head = newNode;
+		return OK;
+	}
+
+	// While the new node is less than the current node and less than the next node
+		// Keep walking down
+	// If we consider the cornercase of head first (head being less than curNode):
+	// We note that by induction, the current node MUST be less than the new node, so only check the next node
+	// If the while-loop breaks => the new node is less than the current node but greater than the next node, so it belongs after the current
+
+
+	if(comparator(newNode->data, curNode->data)==1){ // Corner case - first node
+		newNode->next = curNode;
+		linkedList->head = newNode;
+		return OK;
+	}
+
+	// If there is no next node, just insert
+	while(!(curNode->next==NULL) && comparator(newNode->data, (curNode->next)->data)==-1){
+		curNode = curNode->next;
+	}
+
+	NodePTR temp = curNode->next;
+	curNode->next = newNode;
+	newNode->next = temp;
+
+	return OK;
+}
+
+int PeekHead(LinkedListPTR linkedList, void **data)
+{
+	if(linkedList==NULL){
+		printf("LinkedList pointer is NULL\n");
+		return ERROR;
+	}
+	NodePTR head = linkedList->head;
+	*data = head->data;
+	return OK;
+}
+
+int pop(LinkedListPTR linkedList, void **dataHandle)
+{
+	if(linkedList==NULL){
+		printf("LinkedList pointer is NULL\n");
+		return ERROR;
+	}
+	NodePTR head = linkedList->head;
+
+	linkedList->head = head->next;
+
+	*dataHandle = head->data;
+	
+	free(head);
+
+	linkedList->size = linkedList->size - 1;
+	
+	return OK;
+}
+
+int FindNode(LinkedListPTR linkedList, NodePTR* nodeHandle, void* data)
+{
+	if(linkedList==NULL){
+		printf("LinkedList pointer is NULL\n");
+		return ERROR;	
+	}
+
+	Comparator comparator = linkedList->comparator;
+	NodePTR next = linkedList->head;
+
+	while(next!=NULL){
+		if(comparator(data, next->data)==0){
+			*nodeHandle = next;
+			return FOUND; // Found and assigned
+		}
+		next = next->next;
+	}
+	*nodeHandle = NULL;
+	return NOT_FOUND; // Not found
+}
+
+int DeleteNode(LinkedListPTR linkedList, void* data)
+{
+	if(linkedList==NULL){
+		printf("LinkedList pointer is NULL\n");
+		return ERROR;	
+	}
+
+	NodePTR curNode = linkedList->head;
+
+	if(curNode==NULL){ // If the list is empty
+		return EMPTY_LIST;
+	}
+
+	Comparator comparator = linkedList->comparator;
+
+	// If the next node is the one we are looking for, delete it and assign curNode to it's next
+	
+	if(comparator(data, curNode->data)==0){ // Check head
+		linkedList->head = curNode->next;
+		if(curNode->data==NULL){
+			printf("Node data set to null - modified outside of linked list\n");
+		}else{
+			free(curNode->data);
+		}
+		free(curNode);
+		linkedList->size = linkedList->size - 1;
+		return FOUND;
+	}
+
+	while(!(curNode->next==NULL)){
+		if(comparator(data, curNode->next->data)==0){
+			NodePTR temp = curNode->next->next;
+			if(curNode->next->data==NULL){
+				printf("Node data set to null - modified outside of linked list\n");
+			}else{
+				free(curNode->next->data);
+			}
+			free(curNode->next);
+			curNode->next = temp;
+			linkedList->size = linkedList->size - 1;
+			return FOUND;
+		}
+		curNode = curNode->next;
+	}
+	return NOT_FOUND; // Not found
+}
+
+int PrintLinkedList(LinkedListPTR linkedList)
+{
+	if(linkedList == NULL){
+		printf("LinkedList pointer is NULL\n");
+		return ERROR;
+	}else if(linkedList->head == NULL){
+		printf("List is Empty\n");
+		return EMPTY_LIST;
+	}
+
+	Printer printer = linkedList->printer;
+
+	NodePTR next = linkedList->head;
+	while(next!=NULL){
+		printer(next->data);
+		printf(" ");
+		next = next->next;
+	}
+	printf("\n");
+	return OK;
+}
+
+int SetComparatorLinkedList(LinkedListPTR linkedList, Comparator comparator)
+{
+	if(comparator==NULL){
+		printf("Function pointer to comparator is NULL");
+		return ERROR;
+	}
+	linkedList->comparator = comparator;
+	return OK;
+}
+
+int SetPrinterLinkedList(LinkedListPTR linkedList, Printer printer)
+{
+	if(linkedList == NULL){
+		printf("LinkedList pointer is NULL\n");
+		return ERROR;
+	}
+	linkedList->printer = printer;
+	return OK;
+}
+
+int CreateNode(NodePTR* nodeHandle, void* data, NodePTR next)
+{
+	NodePTR new = (NodePTR) malloc(sizeof(Node));
+	if(new==NULL) return FATAL_ERROR;
+	new->data = data;
+	new->next = next;
+	*nodeHandle = new;
+	return OK;
+}
+
+int DefaultComparator(void* dataOne, void* dataTwo)
+{
+	int* one = (int*) dataOne;
+	int* two = (int*) dataTwo;
+
+	if(two==NULL || *one>*two){
+		return 1;
+	}
+
+	if(*one==*two){
+		return 0;
+	}
+
+	return -1;
+}
+
+void DefaultPrinter(void* data)
+{
+	printf("%d", *((int*)data));
+}
---
+++ linkedlist/LinkedList.h
@@ -0,0 +1,102 @@
+/*
+* LinkedList
+* Author: Abdur-Rahman Ayyaz Qureshi
+* Implementation Notes:
+* 1. This linkedlist takes control and responsibility for freeing the data and setting the pointers equal to null.
+* 2. This linkedlist 'assumes' that the data is integers when printing and comparing nodes. Use SetComparatorLinkedList and SetPrinterLinkedList respectively to
+* override this behaviour.
+* 3. The linkedlist InsertSortedLinkedList method assumes the list is already sorted. It will order the nodes from greatest to lowest according to the comparator.
+* 
+* Notes on 1:
+* The DeleteNode method's implementation will always attempt to delete the internal data as well. So if you are using the actual data to find the node, do not free it!
+* When destroying the linked list, there is a method that will only delete the nodes, but only use it if you have a reference to the nodes, otherwise
+* there will be information leaks. It was never really meant to be used. It is a relic of CSC190.
+* Destroying the nodes (and data) and actual linked list is separated because you may want to clear the list without destroy the actual list.
+* 
+*/ 
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define FATAL_ERROR -1
+#define ERROR 1
+#define OK 0
+#define NOT_FOUND 2
+#define FOUND 3
+#define EMPTY_LIST 4
+
+typedef struct node Node;
+
+typedef Node *NodePTR;
+
+typedef int (*Comparator)(void* dataOne, void* dataTwo);
+
+typedef void (*Printer)(void* data);
+
+struct node {
+	void* data;
+	NodePTR next;
+};
+
+typedef struct {
+	Printer printer;
+	Comparator comparator;
+	NodePTR head;
+	unsigned int size;
+} LinkedList;
+
+typedef LinkedList *LinkedListPTR;
+
+int CreateLinkedList(LinkedListPTR *linkedListHandle);
+
+// Clean the nodes and set size = 0
+int DestroyNodesLinkedList(LinkedListPTR linkedList);
+
+// Clean the nodes and data and set size = 0
+int DestroyNodesAndDataLinkedList(LinkedListPTR linkedList);
+
+// Clean up the linked list struct (not its members) (frees and sets to null)
+int DestroyLinkedList(LinkedListPTR *linkedListHandle);
+
+int CreateNode(NodePTR* nodeHandle, void* data, NodePTR next);
+
+// Appends to the front of the linked list
+int AppendLinkedList(LinkedListPTR linkedList, void *data);
+
+// Inserts the linked list in a sorted order according to the comparator. Inserts in order of greatest to least
+// This function assumes that the nodes are already in order
+int InsertSortedLinkedList(LinkedListPTR linkedList, void* data);
+
+// Stores the front of the linked list in data without removing it
+int PeekHead(LinkedListPTR linkedList, void **data);
+
+// Removes the front of the linked list and assigns it to *dataHandle
+int pop(LinkedListPTR linkedList, void **dataHandle);
+
+// CSC190 Purposes
+// Assigns node to the node in the linkedlist with data 'data' and NULL otherwise
+int FindNode(LinkedListPTR linkedList, NodePTR* nodeHandle, void* data);
+
+// CSC190 Purposes
+// Assigns *nodeHandle the address of the node that is equal to 'data' and null otherwise
+int DeleteNode(LinkedListPTR linkedList, void* data);
+
+// Note: assumes that data is integers
+// TODO: Generalize
+int PrintLinkedList(LinkedListPTR linkedList);
+
+// Returns 1 if dataOne is greater than dataTwo or dataTwo is null
+// Returns 0 if dataOne is equal to dataTwo
+// Returns -1 if dataOne is less than dataTwo
+int SetComparatorLinkedList(LinkedListPTR linkedList, Comparator comparator);
+
+// Printer prints the data element passed to it anyway required
+// Note that spacing between elements will be appended by PrintLinkedList, so printing spaces in the printer function just adds more padding
+// If you want it on separate lines, just use \n in printer
+int SetPrinterLinkedList(LinkedListPTR linkedList, Printer printer);
+
+// For CSC190 purposes, the default comparator assumes data members are integers
+int DefaultComparator(void* dataOne, void* dataTwo);
+
+void DefaultPrinter(void* data);
